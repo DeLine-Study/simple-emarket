a69302a84b365164b7eabf89812c85de1faa7f9b
De_Line <artyomshapovalov030@gmail.com>
2024-05-13 01:18:57 +0400

    Updates

--- assets/router-link-BF16IFSu.js
+++
@@ -1,1 +0,0 @@
-import{j as a,q as t}from"./index-BPMwKi0W.js";const i=s=>a.jsx(t,{...s,className:`link ${s.className}`});export{i as R};
---
+++ assets/router-link-DBaScaJN.js
@@ -0,0 +1,1 @@
+import{j as a,q as t}from"./index-FtfQ2Bpi.js";const i=s=>a.jsx(t,{...s,className:`link ${s.className}`});export{i as R};
